06f37c3ab7b9ae1bea2361e9353f07d8150bd61e
Sebastian Asfaw <sebastianasfaw@gmail.com>
2024-10-26 01:08:26 +0100

    Put file io functions into separate file, renamed sieve.go into main.go and changed initialisation of primes into make

--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
-	"log"
 	"math"
-	"os"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -18,8 +14,10 @@ func sieve(n int) []int {
 
 	primeBool := make([]bool, n+1)
 	//2 is prime, easier to implement skipping even numbers later
-	var primes = []int{2}
+	primes := make([]int, 0)
 
+	primes = append(primes, 2)
+	
 	for i := 3; i <= n; i += 2 {
 		primeBool[i] = true
 	}
@@ -39,78 +37,6 @@ func sieve(n int) []int {
 	}
 
 	return primes
-}
-
-// Can be rewritten to remove lensub1, only done to reduce wasted len()
-func writeToFile(primes []int, lensub1 int) {
-	fileName := "./onef-primes/onef-primes.txt"
-
-	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.FileMode(0644))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	writ := bufio.NewWriter(f)
-
-	fmt.Fprintln(f, "Primes in this file:", lensub1+1)
-
-	fmt.Fprintln(f, fmt.Sprint("Largest: ", primes[lensub1], "\n"))
-
-	for i := range primes {
-		fmt.Fprint(writ, fmt.Sprint(primes[i], ", "))
-	}
-
-	writ.Flush()
-}
-
-func writeToFiles(order int, rangePrimes []int, wg *sync.WaitGroup) {
-	defer wg.Done()
-
-	fileName := fmt.Sprint("primes/primes-" + strconv.Itoa(order) + ".txt")
-
-	f, err := os.OpenFile(fileName, os.O_CREATE|os.O_RDWR|os.O_TRUNC, os.FileMode(0644))
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer f.Close()
-
-	writ := bufio.NewWriter(f)
-
-	fmt.Fprintln(f, "Primes in this file:", len(rangePrimes))
-
-	fmt.Fprintln(writ, fmt.Sprint("Largest: ", rangePrimes[len(rangePrimes)-1], "\n"))
-
-	for i := range rangePrimes {
-		fmt.Fprint(writ, fmt.Sprint(rangePrimes[i], ", "))
-	}
-
-	writ.Flush()
-}
-
-func createPath(onef bool) {
-	var dirName string
-	if onef {
-		dirName = "./onef-primes"
-	} else {
-		dirName = "./primes"
-	}
-
-	err := os.RemoveAll(dirName)
-
-	if err != nil {
-		log.Printf("Error removing directory '%s': %s", dirName, err)
-	}
-
-	err = os.Mkdir(dirName, os.FileMode(0775))
-
-	if err != nil {
-		log.Printf("Error creating directory '%s': %s", dirName, err)
-	}
 }
 
 func setAndParseFlags() (int, int, bool, bool) {
